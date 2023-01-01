28e7123c2c993adbaa6f43df886796878179541a
Jack <JackQuincy@users.noreply.github.com>
2022-12-31 16:00:25 -0800

    tabs -> spaces

--- FFPRSaveEditor.SaveFilesUnitTests/FF1SaveTests.cs
+++ FFPRSaveEditor.SaveFilesUnitTests/FF1SaveTests.cs
@@ -4,18 +4,18 @@ using System.Text.Json;
 
 namespace FFPRSaveEditor.SaveFilesUnitTests
 {
-	[TestClass]
-	public class FF1SaveTests
-	{
-		[TestMethod]
-		public void TestEdit()
+    [TestClass]
+    public class FF1SaveTests
+    {
+        [TestMethod]
+        public void TestEdit()
         {
             string fileData = File.ReadAllText("TestFiles/FF1/7nCxyzTwG31W3Zlg70mo751W8ETH1n+Km0dWOzRU84Y=");
             string jsonData = SaveGame.Decrypt(fileData);
-			var save = FF1Save.Deserialize(jsonData);
-			Assert.IsNotNull(save);
-			var outputJson = save.Serialize();
-			Assert.AreEqual(jsonData.Length, outputJson.Length);
+            var save = FF1Save.Deserialize(jsonData);
+            Assert.IsNotNull(save);
+            var outputJson = save.Serialize();
+            Assert.AreEqual(jsonData.Length, outputJson.Length);
         }
-	}
+    }
 }
